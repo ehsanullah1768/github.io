b6a2c5ed5f7cf7beba987dc320abfd00c1986fbb
a_saadat400@yahoo.com <a_saadat400@yahoo.com>
2019-11-06 18:56:20 +0900

    webdata

---
+++ inc/data.php
@@ -0,0 +1,166 @@
+<?php
+$catalog[101] = [
+	"title" => "Python GUI Programming Cookbook: Use recipes to develop responsive and powerful GUIs using Tkinter, 2nd Edition",
+	"img" => "img/media/pyrhonGUIprogramming.jpg",
+    "genre" => "Programming",
+    "format" => "Textbook",
+    "year" => 2015,
+    "category" => "Books",
+    "authors" => [
+        "Burkhard A. Meier"
+    ],
+    "publisher" => "Packt Publishing",
+    "isbn" => ' 978-1787129450'
+];
+$catalog[102] = [
+    "title" => "Clean Code: A Handbook of Agile Software Craftsmanship",
+    "img" => "img/media/PHPandMYSQL.jpg",
+    "genre" => "Programming",
+    "format" => "Ebook",
+    "year" => 2006,
+    "category" => "Books",
+    "authors" => [
+        "Gilmore, W Jason"
+    ],
+    "publisher" => "Watermarked",
+    "isbn" => '978-1-4302-0117-5'
+];
+$catalog[103] = [
+    "title" => "Windows 8 MVVM Patterns Revealed: covers both C# and JavaScript (Expert's Voice in Windows 8) 1st ed. Edition",
+    "img" => "img/media/Windows8.jpg",
+    "genre" => "Tech",
+    "format" => "EBook",
+    "year" => 2012,
+    "category" => "Books",
+    "authors" => [
+        "Ashish Ghoda"
+    ],
+    "publisher" => "Apress",
+    "isbn" => '978-1430249085'
+];
+$catalog[104] = [
+    "title" => "Microsoft SQL server",
+    "img" => "img/media/SQLserver.jpg",
+    "genre" => "Tech",
+    "format" => "TEXT book",
+    "year" => 2017,
+    "category" => "Books",
+    "authors" => [
+        "Microsoft"
+    ],
+    "publisher" => "Microsoft Corporation",
+    "isbn" => '007-6092046981'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Carawl",
+    "img" => "img/media/Carawl.jpg",
+    "genre" => "Drama",
+    "format" => "DVD",
+    "year" => 2019,
+    "category" => "Movies",
+    "director" => "Alexandre Aja",
+    "writers" => [
+        "Michael Rasmussen",
+		"Shawn Rasmussen"
+    ],
+    "stars" => [
+        "Kaya Scodelario",
+        "Barry Pepper",
+        "Morfydd Clark"
+    ]
+];
+$catalog[202] = [
+    "title" => "Annabelle comes home",
+    "img" => "img/media/Anabela comming home.jpg",
+    "genre" => "Horro,Mystery",
+    "format" => "Blu-ray",
+    "year" => 2019,
+    "category" => "Movies",
+    "director" => "Gary Dauberman",
+    "writers" => [
+        " James Wan "
+    ],
+    "stars" => [
+        " Vera Farmiga ",
+        "Patrick Wilson",
+        " Mckenna Grace"
+    ]
+];
+$catalog[203] = [
+    "title" => "The Lord of the Rings: The Fellowship of the Ring",
+    "img" => "img/media/Loard of the ring.jpg",
+    "genre" => "Drama",
+    "format" => "Blu-ray",
+    "year" => 2001,
+    "category" => "Movies",
+    "director" => "Peter Jackson",
+    "writers" => [
+        "J.R.R. Tolkien",
+        "Fran Walsh",
+        "Philippa Boyens",
+        "Peter Jackson"
+    ],
+    "stars" => [
+        "Ron Livingston",
+        "Jennifer Aniston",
+        "David Herman",
+        "Ajay Naidu",
+        "Diedrich Bader",
+        "Stephen Root"
+    ]
+];
+$catalog[204] = [
+    "title" => "How to Train your Dragon",
+    "img" => "img/media/Dragon.jpg",
+    "genre" => "Animation",
+    "format" => "DVD",
+    "year" => 2019,
+    "category" => "Movies",
+    "director" => "Dean DeBlois",
+    "writers" => [
+        " Dean DeBlois, Cressida Cowell"
+    ],
+    "stars" => [
+        " Jay Baruchel ",
+        "America Ferrera",
+        "F. Murray Abraham"
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "My heart does go on",
+    "img" => "img/media/My heart.jpg",
+    "genre" => "Clasical",
+    "format" => "CD",
+    "year" => 1997,
+    "category" => "Music",
+    "artist" => "Celine Dion"
+];
+$catalog[302] = [
+    "title" => "No sorry",
+    "img" => "img/media/Nosorry.jpg",
+    "genre" => "pop",
+    "format" => "MP3",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Demi Lovato"
+];
+$catalog[303] = [
+    "title" => "Kill this love",
+    "img" => "img/media/Blackpink.jpg",
+    "genre" => "K-pop, Hip hop ",
+    "format" => "Cassette",
+    "year" => 2019,
+    "category" => "music",
+    "artist" => "BLACKPINK"
+];
+$catalog[304] = [
+    "title" => "The heartbeat",
+    "img" => "img/media/Heartbeat.jpg",
+    "genre" => "pop",
+    "format" => "MP3",
+    "year" => 2010,
+    "category" => "Music",
+    "artist" => "Enrique Iglesias"
+];
